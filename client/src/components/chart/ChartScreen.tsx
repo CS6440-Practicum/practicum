5b66f747b343c708fbaf85043f6820d104775664
Evan Strat <evan10s@users.noreply.github.com>
2021-04-18 21:03:24 -0400

    add very jank glucose chart

--- client/src/components/chart/ChartScreen.tsx
+++ client/src/components/chart/ChartScreen.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import DataChart from "./DataChart";
+import useAsyncEffect from "use-async-effect";
+import LoadingSpinner from "../util/LoadingSpinner";
 
 const ChartScreen = () => {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
+	// Intl.DateTimeFormat().resolvedOptions().timeZone
+	// https://stackoverflow.com/a/34602679
+	useAsyncEffect(async () => {
+		const dataReq = await fetch("/api/mergedData?start=2021-04-18T00:00:00Z&end=2021-04-18T02:00:00Z&tz=America/New_York#");
+		const json = await dataReq.json();
+		setLoading(false);
+		setData(json.data);
+	}, [])
+
+	if (loading) {
+		return <LoadingSpinner content={"Loading data..."} />
+	}
+
 	return (
 		<div>
-			<DataChart />
+			<DataChart data={data}/>
 		</div>
 	);
 };
--- client/src/components/chart/DataChart.tsx
+++ client/src/components/chart/DataChart.tsx
@@ -1,54 +1,35 @@
 import React from "react";
-import XYFrame from "semiotic/lib/XYFrame"
+import {scaleTime} from "d3-scale";
+import {ResponsiveXYFrame} from "semiotic";
 
-const DataChart = () => {
-	/**
-	 * Based on sample code provided in the Semiotic docs at https://semiotic.nteract.io/guides/scatterplot
-	 */
-	const frameProps = {
-		/* --- Data --- */
-		points: [{ theaterCount: 4, rank: 18, grossWeekly: 327616, title: "Ex Machina" },
-			{ theaterCount: 39, rank: 15, grossWeekly: 1150814, title: "Ex Machina" } ],
+interface DataChartProps {
+	data: any, //TODO
+}
 
-		/* --- Size --- */
-		size: [700,400],
-		margin: { left: 60, bottom: 90, right: 10, top: 40 },
-
-		/* --- Process --- */
-		xAccessor: "theaterCount",
-		yAccessor: "rank",
-		yExtent: [0],
-		xExtent: [0],
-
-		/* --- Customize --- */
-		pointStyle: (d: any) => {
-			return {
-				r: 5,
-				fill:
-					d.title === "Ex Machina"
-						? "#ac58e5"
-						: d.title === "Far from the Madding Crowd"
-						? "#E0488B"
-						: "#9fd0cb"
-			}
-		},
-		title: (
-			<text textAnchor="middle">
-				Theaters showing <tspan fill={"#ac58e5"}>Ex Machina</tspan> vs{" "}
-				<tspan fill={"#E0488B"}>Far from the Madding Crowd</tspan>
-			</text>
-		),
-		axes: [{ orient: "left", label: "Rank" },
-			{ orient: "bottom", label: { name: "Theaters", locationDistance: 55 } }]
-	}
-
+const DataChart = ({data}: DataChartProps) => {
 	// @ts-ignore
-	return (
-		<div>
-			{/*@ts-ignore*/}
-			<XYFrame {...frameProps} />
-		</div>
-	);
+	return <ResponsiveXYFrame
+		responsiveWidth={true}
+		axes={[
+			{
+				orient: "left",
+				// @ts-ignore
+				label: "Blood sugar (mg/dL)"
+			},
+			{
+				orient: "bottom",
+				// @ts-ignore
+				label: "Time",
+				tickFormat: (d: any) => d.getHours() + ":" + d.getMinutes()
+			}
+		]}
+		points={data}
+		xAccessor={(d: any) => new Date(d.timestamp).getTime()}
+		yAccessor="egv"
+		// @ts-ignore
+		xScaleType={scaleTime()}
+		yExtent={[40, 400]}
+	/>;
 };
 
 export default DataChart;
